67b0b07ca86b7adab688777cbd20d990bfd6f30d
Mohd Arman <armansheakh987@gmail.com>
2024-10-25 10:59:24 +0530

    learning react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import LifecycleA from "./components/LifecycleA";
+import FragmentDemo from "./components/FragmentDemo";
+import Tables from "./components/Tables";
+import PureComp from "./components/PureComp";
+import ParentComp from "./components/ParentComp";
+import MemoComp from "./components/MemoComp";
+import RefsDemo from "./components/RefsDemo";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <LifecycleA/> */}
+      {/* <FragmentDemo/> */}
+      {/* < Tables /> */} 
+      {/* <ParentComp /> */}
+      {/* <PureComp /> */}
+      {/* <MemoComp /> */}
+      <RefsDemo />
     </div>
   );
 }
